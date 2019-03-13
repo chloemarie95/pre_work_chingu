c0b8b88a1d966ce53e395d96f63d20072dcf724e
Chloe McDermott <chloemcdermott95@aol.com>
2019-03-13 22:03:56 +0000

    amendments

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,22 @@ import Splitting from "splitting";
 
 Splitting();
 
+
+// Fade in button to scroll to top - not working but not sure why
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#scroll').fadeIn();
+    } else {
+        $('#scroll').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#scroll").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
